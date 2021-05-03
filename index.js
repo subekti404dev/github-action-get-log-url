12e459534da4cf5fd08af85c51045561476e9bb5
Urip Subekti <urip.subekti13@gmail.com>
2021-05-03 08:15:31 +0700

    initials

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require("@actions/core");
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+
+
+const GITHUB = core.getInput("github_context");
+
+const getHTML = async () => {
+   const url = `https://github.com/${GITHUB.repository}/actions/runs/${GITHUB.run_id}`;
+   const res = await axios.get(url)
+   return res.data
+}
+
+const main = async () => {
+   const html = await getHTML();
+   const $ = cheerio.load(html);
+   const urls = [];
+   $('.WorkflowJob-title').each((_i1, e1) => {
+      urls.push(`https://github.com${$(e1).attr('href')}`)
+   });
+   core.setOutput("result", urls[0]);
+}
+
+main()
